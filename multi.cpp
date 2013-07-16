aafdb61d0c1cc17f8bcc2abf6980cd9171dd8999
Kisuka <kisuka@kisuka.com>
2013-07-15 21:00:01 -0700

    Updated Common interface to use RSU_PLUG_VERSION2.

--- multi.cpp
+++ multi.cpp
@@ -184,7 +184,7 @@ bool __stdcall Plugin_Init(PlugInterfaceQueryFunc lpInterfaceQuery, PlugInterfac
 	// pass notification function
 	*lppOnEvent = &Plugin_OnEvent;
 
-	if(!Plug_InterfaceQuery(RSU_PLUG_VERSION1, RSU_PLUG_TYPE_COMMON, NULL, (void**)&lpCommon))
+	if(!Plug_InterfaceQuery(RSU_PLUG_VERSION2, RSU_PLUG_TYPE_COMMON, NULL, (void**)&lpCommon))
 	{
 		return false;
 	}
